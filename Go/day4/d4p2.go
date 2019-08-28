c8322077fe661dc2f25da4121413dcf3e67be0db
Blake Loewengruber <ambsneakysnack@gmail.com>
2019-08-28 15:25:18 -0400

    Add day4 Go solution

---
+++ Go/day4/d4p2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "crypto/md5"
+    "strconv"
+    "strings"
+    "encoding/hex"
+)
+
+type Coord struct {
+    x, y int
+}
+
+type Void struct {}
+
+func main() {
+    // Get input
+    input := "iwrupvqb"
+
+    number := 1
+
+    sixZeros := "000000"
+
+    for {
+        numStr := strconv.Itoa(number)
+        data := []byte(input + numStr)
+        sum := md5.Sum(data)
+        encoded := hex.EncodeToString(sum[:])
+        // fmt.Printf("%s %s\n", string(data), encoded)
+        if strings.HasPrefix(encoded, sixZeros) {
+            break
+        }
+        number++
+    }
+
+    fmt.Printf("Number: %d\n", number)
+
+}
